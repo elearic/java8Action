5ae54debac49aefab478bf452ce62b078497ee6f
elearic <elearic@aliyun.com>
2019-02-04 23:52:55 +0800

    add Supplier demo

---
+++ src/main/java/com/ericzz/functionInteface/SupplierTestDemo.java
@@ -0,0 +1,40 @@
+/**
+ *
+ *  Bestpay.com.cn Inc.
+ *  Copyright (c) 2011-2019 All Rights Reserved.
+ */
+package com.ericzz.functionInteface;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import java.util.function.Supplier;
+
+/** 
+ * 
+  * @author zz_huns  
+ * @version Id: SupplierTestDemo.java, v 0.1 2019/2/4 11:49 PM zz_huns Exp $$
+  */
+public class SupplierTestDemo {
+
+    //java.util.Function.Supplier<T> 具有唯一一个抽象方法叫做get,
+    //代表的函数描述符是 () -> T, 
+    Supplier<Apple> supplier = () -> new Apple();
+
+
+
+}
+
+
+class Apple {
+
+    private String color;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
