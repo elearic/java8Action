813af4d5e40864057efad55585e390ef38dc5b3d
elearic <elearic@aliyun.com>
2019-02-07 18:30:41 +0800

    fix

--- src/main/java/com/ericzz/part/two/stream/StreamTestDemo.java
+++ src/main/java/com/ericzz/part/two/stream/StreamTestDemo.java
@@ -58,16 +58,47 @@ public class StreamTestDemo {
 
         //---------------------筛选和切片---------------------------------
 
-        //用谓词筛选：
+        //1.用谓词筛选：
 
         //Stream接口支持filter。该操作会接受一个谓词(一个返回boolean的函数)作为参数，并才返回
         //所有符合谓词的元素的流。
         List<Banana> bananas1 = bananas.stream()
-                .filter(banana -> banana.getWeight() > 300 ) //谓词判断筛选出符合条件的元素
-                .collect(toList());
+                                       .filter(banana -> banana.getWeight() > 300 ) //谓词判断筛选出符合条件的元素
+                                       .collect(toList());
 
-        //筛选各异的元素
+        //2.筛选各异的元素
 
-        //流还支持一个叫做distince的方法，它会
+        //流还支持一个叫做distinct的方法，它会返回一个元素各异(根据流所生成元素的hashCode和equals方法实现)
+        //的流
+        //例如，以下代码会筛选出列表中所有的偶数，并确保没有重复
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,1,2,4,5);
+        numbers.stream()
+                .filter(integer -> integer % 2 == 0)
+                .distinct()
+                .forEach(System.out::println);
+
+        //3.截断流
+
+        //流支持limit(n)方法，该方法会返回一个不超过给定长度的流。所需的长度作为参数传递给limit
+        //如果留是有序的，则最多会返回前n个元素。
+        //例如，建立一个list,选出重量超过300的前3个
+        List<Banana> list2 = bananas.stream()
+                                    .filter(banana -> banana.getWeight() > 300)
+                                    .limit(3)
+                                    .collect(toList());
+
+        //4.跳过元素
+        //流还支持skip(n)方法，返回一个扔掉了前N个元素的流。如果流中元素不足n个，则返回一个空流。
+        //注意: limit(n)和skip(n)是互补的
+        //例如，建立一个list,选出重量超过300的前3个剩下的
+        List<Banana> bananas2 = bananas.stream()
+                                       .filter(banana -> banana.getWeight() > 300)
+                                       .skip(3)
+                                       .collect(toList());
+
+        //---------------------映射---------------------------------
+        
+
+
     }
 }
